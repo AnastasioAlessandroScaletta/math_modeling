78d54064649636fb6e7b3aa4b7eaaea67139c0d3
a.a.scaletta <91831713+AnastasioAlessandroScaletta@users.noreply.github.com>
2021-11-25 18:41:35 +0000

    бу.

--- lec_3_generators.py
+++ lec_3_generators.py
@@ -1,4 +1,4 @@
-import numpy as numpy
+import numpy as np
 
 a = range(0, 5, 1)
 print(a)
--- lec_3_import_as.py
+++ lec_3_import_as.py
@@ -2,7 +2,7 @@ import lec_3_my_module as mm
 
 print(mm.a)
 
-b = mm.b
+b = mm.b * 3
 print(b)
 
 print(mm.c[2] + b + mm.c[0])
